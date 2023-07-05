106697aac3cde2d1c2ccfc12f9921189440cdcf3
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-07-05 22:55:53 +0700

    Update SignUpActivity.kt

--- Day2/app/src/main/java/com/codingstuff/loginandsignup/SignUpActivity.kt
+++ Day2/app/src/main/java/com/codingstuff/loginandsignup/SignUpActivity.kt
@@ -37,30 +37,18 @@ class SignUpActivity : AppCompatActivity() {
 
             if (name.equals("") || email.equals("") || programStudy.equals("") || semester.equals("")) {
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-            }
-            else if (!isEmailValid(email)){
+            } else if (!isEmailValid(email)){
                 Toast.makeText(this, "Your email is invalid !!", Toast.LENGTH_SHORT).show()
+            } else{
+                firebaseAuth.createUserWithEmailAndPassword(name, email).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, SignUpActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Succeed !!", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
-
-//            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
-//                if (pass == confirmPass) {
-//
-//                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
-//                        if (it.isSuccessful) {
-//                            val intent = Intent(this, SignInActivity::class.java)
-//                            startActivity(intent)
-//                        } else {
-//                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-//
-//                        }
-//                    }
-//                } else {
-//                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
-//                }
-//            } else {
-//                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-//
-//            }
         }
     }
 }
