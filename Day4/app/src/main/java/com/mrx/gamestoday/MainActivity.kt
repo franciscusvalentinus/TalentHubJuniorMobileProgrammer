7710088cfc6b82010eec13a8f8c9f19271193c8c
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-07-07 23:49:54 +0700

    create day4

---
+++ Day4/app/src/main/java/com/mrx/gamestoday/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.mrx.gamestoday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mrx.gamestoday.adapter.ListCharAdapter
+import com.mrx.gamestoday.db.CharData
+import com.mrx.gamestoday.model.GenshinChar
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvChar: RecyclerView
+    private var list: ArrayList<GenshinChar> = arrayListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val actionbar = supportActionBar
+        actionbar!!.title = "Data Mahasiswa"
+
+        rvChar = findViewById(R.id.rv_char)
+        rvChar.setHasFixedSize(true)
+
+        list.addAll(CharData.listData)
+        rvChar.layoutManager = LinearLayoutManager(this)
+        val listCharAdapter = ListCharAdapter(list)
+        rvChar.adapter = listCharAdapter
+    }
+}
