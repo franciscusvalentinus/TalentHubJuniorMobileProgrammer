94f722b41b14bdde7c35960e6b8f27e9135a7588
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-07-07 08:36:23 +0700

    Update SplashActivity.kt

--- Day3/app/src/main/java/com/mrx/gamestoday/activity/SplashActivity.kt
+++ Day3/app/src/main/java/com/mrx/gamestoday/activity/SplashActivity.kt
@@ -14,6 +14,6 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 2000)
+        }, 3000)
     }
 }
