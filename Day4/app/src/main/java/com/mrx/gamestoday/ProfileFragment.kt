3bd651e0609771362accf30f84e4ffcf826cf2a1
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-07-08 10:00:12 +0700

    done

--- Day4/app/src/main/java/com/mrx/gamestoday/ProfileFragment.kt
+++ Day4/app/src/main/java/com/mrx/gamestoday/ProfileFragment.kt
@@ -5,13 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mrx.gamestoday.adapter.ListCharAdapter
+import com.mrx.gamestoday.db.CharData
+import com.mrx.gamestoday.model.GenshinChar
 
 class ProfileFragment : Fragment() {
+    private lateinit var rvChar: RecyclerView
+    private var list: ArrayList<GenshinChar> = arrayListOf()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rvChar = view.findViewById(R.id.rv_char)
+        rvChar.setHasFixedSize(true)
+
+        list.addAll(CharData.listData)
+        rvChar.layoutManager = LinearLayoutManager(context)
+        val listCharAdapter = ListCharAdapter(list)
+        rvChar.adapter = listCharAdapter
     }
 }
